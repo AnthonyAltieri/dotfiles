b07e84976693dfd1b6db0cb42c12fc9515f19bed
Anthony Altieri <anthonyraltieri@gmail.com>
2024-03-01 11:20:10 -0500

    feat(nvim): cmp not lazy loaded

--- .config/nvim/lua/aalt/lazy/lsp.lua
+++ .config/nvim/lua/aalt/lazy/lsp.lua
@@ -12,7 +12,7 @@ return {
     "saadparwaiz1/cmp_luasnip",
     "j-hui/fidget.nvim",
   },
-
+  lazy = false,
   config = function()
     local cmp = require('cmp')
     local cmp_lsp = require("cmp_nvim_lsp")
