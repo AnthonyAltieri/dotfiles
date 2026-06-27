fdde816f1bc1da6e6b2b356432918dd805a6a6bb
Anthony Altieri <anthonyraltieri@gmail.com>
2026-06-27 17:09:29 -0400

    fix(ghostty): manage macos app config

--- modules/platforms/darwin/ghostty.nix
+++ modules/platforms/darwin/ghostty.nix
@@ -1,8 +1,13 @@
 { overwriteHomeManagerBackups ? false, ... }:
-{
-  xdg.configFile."ghostty" = {
+let
+  ghosttyConfig = {
     source = ../../../home/.config/ghostty;
     recursive = true;
     force = overwriteHomeManagerBackups;
   };
+in
+{
+  xdg.configFile."ghostty" = ghosttyConfig;
+
+  home.file."Library/Application Support/com.mitchellh.ghostty" = ghosttyConfig;
 }
