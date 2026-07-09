bc69ec06ceb80d397d2e81e1d6e3043c54dcd6a8
Anthony Altieri <AnthonyAltieri@users.noreply.github.com>
2026-07-09 13:54:22 -0400

    fix(nvim): install treesitter language queries

--- tests/nvim-treesitter-config-smoke.sh
+++ tests/nvim-treesitter-config-smoke.sh
@@ -13,8 +13,13 @@ mkdir -p "$CURRENT_LUA/nvim-treesitter" "$LEGACY_LUA/nvim-treesitter"
 cat >"$CURRENT_LUA/nvim-treesitter/init.lua" <<'EOF'
 local M = {}
 
-function M.setup()
+function M.setup(opts)
 	_G.current_treesitter_setup_called = true
+	_G.current_treesitter_setup_opts = opts
+end
+
+function M.install(languages)
+	_G.current_treesitter_install_languages = languages
 end
 
 function M.indentexpr()
@@ -52,7 +57,7 @@ TREESITTER_SPEC="$ROOT_DIR/home/.config/nvim/lua/aalt/lazy/treesitter.lua"
 
 NVIM_LOG_FILE="$TMP_DIR/current-nvim.log" nvim --clean --headless -i NONE \
 	+"lua package.path = '$CURRENT_LUA/?.lua;$CURRENT_LUA/?/init.lua;' .. package.path" \
-	+"lua local ok, err = xpcall(function() local registered = {}; vim.treesitter.start = function() _G.current_treesitter_started = true end; vim.treesitter.language.register = function(parser, filetype) registered[filetype] = parser end; local spec = dofile('$TREESITTER_SPEC'); spec.config(); assert(_G.current_treesitter_setup_called == true); assert(require('nvim-treesitter.parsers').templ.install_info.branch == 'master'); assert(registered.templ == 'templ'); assert(registered.typescriptreact == 'tsx'); vim.bo.filetype = 'typescriptreact'; vim.api.nvim_exec_autocmds('FileType', { pattern = 'typescriptreact' }); assert(_G.current_treesitter_started == true); assert(vim.bo.indentexpr == [[v:lua.require'nvim-treesitter'.indentexpr()]]) end, debug.traceback); if not ok then io.stderr:write(err .. '\n'); vim.cmd('cquit') end" \
+	+"lua local ok, err = xpcall(function() local registered = {}; vim.treesitter.start = function() _G.current_treesitter_started = true end; vim.treesitter.language.register = function(parser, filetype) registered[filetype] = parser end; local spec = dofile('$TREESITTER_SPEC'); spec.config(); assert(_G.current_treesitter_setup_called == true); assert(_G.current_treesitter_setup_opts.install_dir == vim.fn.stdpath('data') .. '/site'); assert(vim.tbl_contains(_G.current_treesitter_install_languages, 'typescript')); assert(vim.tbl_contains(_G.current_treesitter_install_languages, 'tsx')); assert(require('nvim-treesitter.parsers').templ.install_info.branch == 'master'); assert(registered.templ == 'templ'); assert(registered.typescriptreact == 'tsx'); vim.bo.filetype = 'typescriptreact'; vim.api.nvim_exec_autocmds('FileType', { pattern = 'typescriptreact' }); assert(_G.current_treesitter_started == true); assert(vim.bo.indentexpr == [[v:lua.require'nvim-treesitter'.indentexpr()]]) end, debug.traceback); if not ok then io.stderr:write(err .. '\n'); vim.cmd('cquit') end" \
 	+qa
 
 NVIM_LOG_FILE="$TMP_DIR/legacy-nvim.log" nvim --clean --headless -i NONE \
